39f52cd9c45d109a3a8b03034acbc219e4322dec
StefHoeben <sth@scarlet.be>
2020-06-25 16:52:47 +0200

    Added javadoc + changed the typeof some params

--- esealing-ws/src/main/java/com/zetes/projects/bosa/esealing/model/ListRequest.java
+++ esealing-ws/src/main/java/com/zetes/projects/bosa/esealing/model/ListRequest.java
@@ -1,19 +1,32 @@
 package com.zetes.projects.bosa.esealing.model;
 
+/**
+ * See https://www.etsi.org/deliver/etsi_ts/119400_119499/119432/01.01.01_60/ts_119432v010101p.pdf 2019-03, par 8.5.1
+ * See https://forge.etsi.org/rep/esi/x19_432_sign_creation_protocol/raw/v1.1.1/19432-openapi.yaml - signing-certificates-list-request
+ */
 public class ListRequest {
 
     private String requestID;
     private String lang;
-    private String[] certificates;
+    private String certificates;
     private Boolean certInfo;
-    private String authInfo;
+    private Boolean authInfo;
     private String profile;
     private String signerIdentity;
 
     public ListRequest() {
     }
 
-    public ListRequest(String requestID, String lang, String[] certificates, Boolean certInfo, String authInfo, String profile, String signerIdentity) {
+    /**
+     * @param requestID       not relevant (only in case of asynchronous communication) - see par 7.3
+     * @param lang            requested response language, e.g. "en", "nl", "fr", "de" - see par 7.9
+     * @param certificates    which certificates should be returned, allowed values: are "none", "single" (= default), "chain" - see par 7.10
+     * @param certInfo        true if info about the end entity certs should be included as printable strings, false (= default) otherwise - see par 7.10
+     * @param authInfo        true if info on the authorization mechanisms should be included, false (= default) otherwise - see par 7.10
+     * @param profile         not relvant, set to null - see par 7.15
+     * @param signerIdentity  unique ID to identify the signer - see par 7.17
+     */
+     public ListRequest(String requestID, String lang, String certificates, Boolean certInfo, Boolean authInfo, String profile, String signerIdentity) {
         this.requestID = requestID;
         this.lang = lang;
         this.certificates = certificates;
@@ -39,11 +52,11 @@ public class ListRequest {
         this.lang = lang;
     }
 
-    public String[] getCertificates() {
+    public String getCertificates() {
         return certificates;
     }
 
-    public void setCertificates(String[] certificates) {
+    public void setCertificates(String certificates) {
         this.certificates = certificates;
     }
 
@@ -55,11 +68,11 @@ public class ListRequest {
         this.certInfo = certInfo;
     }
 
-    public String getAuthInfo() {
+    public Boolean getAuthInfo() {
         return authInfo;
     }
 
-    public void setAuthInfo(String authInfo) {
+    public void setAuthInfo(Boolean authInfo) {
         this.authInfo = authInfo;
     }
 
