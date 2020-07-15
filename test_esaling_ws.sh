1dfcf5edcbcfd5181d41917c3f9a777f9937a069
Wouter Verhelst <wouter.verhelst@zetes.com>
2020-07-15 15:22:26 +0200

    Also allow overriding whether or not to use encryption

--- test_esaling_ws.sh
+++ test_esaling_ws.sh
@@ -2,14 +2,15 @@ export HOST=${HOST:-localhost}
 export PORT=${PORT:-8080}
 export USERNAME=${USERNAME:-selor}
 export PASS=${PASS:-test123}
+export PROTO=${PROTO:-http}
 
 printf "==== List request ====\n"
-curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json http://$HOST:$PORT/esealing/credentials/list
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json https://$HOST:$PORT/esealing/credentials/list
 
 printf "\n\n==== Info request ====\n"
-curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json http://$HOST:$PORT/esealing/credentials/info
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json https://$HOST:$PORT/esealing/credentials/info
 
 printf "\n\n==== Sign request ====\n"
-curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json http://$HOST:$PORT/esealing/signatures/signHash
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json https://$HOST:$PORT/esealing/signatures/signHash
 
 printf "\n\n"
