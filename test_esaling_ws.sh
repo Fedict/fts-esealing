9d2c8cd2c0e0cf7bdcfc87b4bb3056fc3ae96884
Wouter Verhelst <wouter.verhelst@zetes.com>
2020-07-15 15:20:38 +0200

    Don't override variables if they're already defined
    Also, don't reuse $PWD, which is a predefined variable in bash for the
    current working directory...

--- test_esaling_ws.sh
+++ test_esaling_ws.sh
@@ -1,15 +1,15 @@
-export HOST=localhost
-export PORT=8080
-export USERNAME=selor
-export PWD=test123
+export HOST=${HOST:-localhost}
+export PORT=${PORT:-8080}
+export USERNAME=${USERNAME:-selor}
+export PASS=${PASS:-test123}
 
 printf "==== List request ====\n"
-curl -X POST --user $USERNAME:$PWD -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json http://$HOST:$PORT/esealing/credentials/list
+curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json http://$HOST:$PORT/esealing/credentials/list
 
 printf "\n\n==== Info request ====\n"
-curl -X POST --user $USERNAME:$PWD -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json http://$HOST:$PORT/esealing/credentials/info
+curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json http://$HOST:$PORT/esealing/credentials/info
 
 printf "\n\n==== Sign request ====\n"
-curl -X POST --user $USERNAME:$PWD -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json http://$HOST:$PORT/esealing/signatures/signHash
+curl -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json http://$HOST:$PORT/esealing/signatures/signHash
 
 printf "\n\n"
