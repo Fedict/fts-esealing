28b67df7e6f13cc03e6cd25f9069c70247db24bb
stef.houben <stef.hoeben@zetes.com>
2020-07-31 10:29:21 +0200

    Typo fix in test_esealing_ws.sh

--- test_esaling_ws.sh
+++ test_esaling_ws.sh
@@ -5,12 +5,12 @@ export PASS=${PASS:-test123}
 export PROTO=${PROTO:-http}
 
 printf "==== List request ====\n"
-curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json https://$HOST:$PORT/esealing/credentials/list
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/listreq.json $PROTO://$HOST:$PORT/esealing/credentials/list
 
 printf "\n\n==== Info request ====\n"
-curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json https://$HOST:$PORT/esealing/credentials/info
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/inforeq.json $PROTO://$HOST:$PORT/esealing/credentials/info
 
 printf "\n\n==== Sign request ====\n"
-curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json https://$HOST:$PORT/esealing/signatures/signHash
+curl -k -X POST --user $USERNAME:$PASS -H "Content-type: application/json; charset=UTF-8" --data-binary @esealing-ws/samples/signreq.json $PROTO://$HOST:$PORT/esealing/signatures/signHash
 
 printf "\n\n"
